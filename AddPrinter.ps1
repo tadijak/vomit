d4fdb110411a3cf671742b95de6f67bfcc7a6344
Tadija Kovacevic <tadija.kovacevic@mpcgroup.com.au>
2018-07-15 13:18:59 +1000

    this is my first script

---
+++ AddPrinter.ps1
@@ -0,0 +1,11 @@
+﻿$printserver = "MPCSUHSVR01"
+$printers = Get-Printer -ComputerName $printserver
+
+foreach ($printer in $printers)
+{
+    
+    $printer_path = "\\" + $printer.ComputerName + "\" + $printer.Name
+    Add-Printer -ConnectionName $printer_path 
+    
+     
+}
